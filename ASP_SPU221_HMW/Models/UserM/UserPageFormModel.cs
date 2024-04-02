621149a361dffd2cbe7598da2deddba05f3c88d6
Anahaiell <anahielit@gmail.com>
2024-04-02 13:51:13 +0300

    first hmw

---
+++ ASP_SPU221_HMW/Models/UserM/UserPageFormModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_SPU221_HMW.Models.UserM
+{
+    public class UserPageFormModel
+    {
+        [FromForm(Name = "username-nick")]
+        public String UserNick {  get; set; }
+        [FromForm(Name = "user-email")]
+        public String UserEmail { get; set; }
+        [FromForm(Name = "user-password1")]
+        public String UserPassword { get; set; }
+    }
+}
---
+++ ASP_SPU221_HMW/Models/UserM/UserPage_PageModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ASP_SPU221_HMW.Models.UserM
+{
+    public class UserPage_PageModel
+    {
+        public String PageTitle {  get; set; }
+        public UserPageFormModel? Model { get; set; }
+    }
+}
